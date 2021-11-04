671937df8309afacda2e55abba8bd67b4c3299f7
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2021-11-04 12:04:59 +0100

    Update misc.jl

--- misc.jl
+++ misc.jl
@@ -129,4 +129,5 @@ par_C30 = GasChromatographySimulator.Parameters(sys_C30, prog_C30, sub_C30, opt_
 
 tR_lock_C30 = 60.0
 tR_tol_C30 = 1e-3
-n = GasChromatographyTools.RT_locking(par_C30, tR_lock_C30, tR_tol_C30, "C30")
+n = GasChromatographyTools.RT_locking(par_C30, tR_lock_C30, tR_tol_C30, "C30"; opt_itp="linear")
+n2 = GasChromatographyTools.RT_locking(par_C30, tR_lock_C30, tR_tol_C30, "C30"; opt_itp="spline")
